d5c84e881595da17b366633ad59bceb7ccfcd808
Aymen Torche(le bg) <aymentrc3@gmail.com>
2026-04-07 13:52:24 +0200

    fusion classes

--- core/spring-boot/src/main/java/org/springframework/boot/context/config/ConfigDataEnvironmentContributors.java
+++
@@ -1,3 +0,0 @@
-// Ce fichier a été supprimé suite au refactoring architectural 
-// La logique a été entièrement fusionnée dans ConfigDataEnvironment sous forme d'une Inner Class native (Contributors).
-// SRP respecté et fragmentation éliminée.
